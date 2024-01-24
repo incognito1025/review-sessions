bd7e056c37c7de93261cf7ed5ea2a61283b13fdd
incognito1025 <nkhanamart@gmail.com>
2024-01-23 19:54:35 -0500

    Problem 1 - not logging properly

---
+++ index_1_23.js
@@ -0,0 +1,50 @@
+// 1.----------------------------------
+// Write a function that takes in a string and returns a new string with every word capitalized. Write your own examples to call the function with.
+
+function everyWordCapitalized(string) {
+    
+
+    for (let i = 0; i < words.length; i++) {
+        words[i].toUpperCase() + words[i].substr(1);
+    }
+words.join(" ");
+}
+const sentence = "Let's study now!";
+const words = sentence.split(" ");
+
+const capitalizedSentence = everyWordCapitalized(string);
+console.log(capitalizedSentence("cap every word."));
+
+
+/*
+// 2.----------------------------------
+// Write a function that takes in an object and returns a formatted greeting.
+// This is the format of the object - 
+
+
+const john = {
+    firstName: "John",
+    lastName: "Klaus",
+    occupation: "Carpenter"
+  }
+  
+  const rebecca = {
+    firstName: "Rebecca",
+    lastName: "Cohen",
+    occupation: "Teacher"
+  }
+  
+  console.log(formatGreeting(john) === "Hello John Klaus, have a great day at your job as a Carpenter")
+  
+  console.log(formatGreeting(rebecca) === "Hello Rebecca Cohen, have a great day at your job as a Teacher")
+  
+  // 3.----------------------------------
+  // Write a function that takes an array of objects and a targetId and returns a formatted string. Each object in the array will have the same structure as the objects from the previous question plus an id. Use chatGPT to create an array of 10 such objects.
+  
+  /* Example Object
+  {
+    id: "Tfjso_4M"
+    firstName: "John",
+    lastName: "Klaus",
+    occupation: "Carpenter"
+  }
